74cc2ea88774a70c5c129ef866dae335306c7ff9
micha <michabl@metropolia.fi>
2025-10-29 09:41:00 +0200

    first commit

---
+++ src/main/java/Tehtava1/test1.java
@@ -0,0 +1,33 @@
+package Tehtava1;
+
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter the number of items they want to purchase");
+        int product=sc.nextInt();
+
+        for (int i=1;i<=product;i++){
+            int pres = sc.nextInt();
+            System.out.println("enter items");
+            int item=sc.nextInt();
+            System.out.println("Entrer precie");
+            int price=sc.nextInt();
+
+
+
+
+
+        }
+
+    }
+
+
+
+}
+
+
